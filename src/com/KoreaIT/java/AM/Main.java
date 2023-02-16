9a6ac2bc1059907059b1899b3cb64c1b44fbdc75
Inha921119 <dlsgk921119@gmail.com>
2023-02-16 11:30:26 +0900

    중간 세이브

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -81,8 +81,97 @@ public class Main {
 				}
 				System.out.println("번호 : " + foundArticle.id);
 				System.out.println("날짜 : " + foundArticle.NowDate + " " + foundArticle.NowTime);
+				if (foundArticle.LastEditDate != null) {
+					System.out.println("수정된 날짜 : " + foundArticle.LastEditDate + " " + foundArticle.LastEditTime);	
+				}
 				System.out.println("제목 : " + foundArticle.title);
 				System.out.println("내용 : " + foundArticle.body);
+
+			} else if (command.startsWith("article delete")) {
+				if (command.split(" ").length != 3) {
+					System.out.println("delete 번호를 입력해주세요");
+					continue;
+				}
+				String cmdBits = command.split(" ")[2];
+				int id = Integer.parseInt(cmdBits);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				articles.remove(id - 1);
+
+				System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
+			} else if (command.startsWith("article edit")) {
+				if (command.split(" ").length != 3) {
+					System.out.println("edit 번호를 입력해주세요");
+					continue;
+				}
+				String cmdBits = command.split(" ")[2];
+				int id = Integer.parseInt(cmdBits);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				System.out.printf("%d번 게시물의 제목과 내용중 무엇을 수정하시겠습니까?\n", id);
+				String command_edit = sc.nextLine().trim();
+				if (command_edit.equals("제목")) {
+					System.out.printf("%d번 게시물의 제목을 수정합니다\n", id);
+					System.out.printf("제목 : ");
+					String title = sc.nextLine();
+					
+					foundArticle.title = title;
+					
+					LocalDate lastdate = LocalDate.now();
+					String LastEditDate = lastdate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+					LocalTime lasttime = LocalTime.now();
+					String LastEditTime = lasttime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+					
+					foundArticle.LastEditDate = LastEditDate;
+					foundArticle.LastEditTime = LastEditTime;
+					
+					System.out.printf("%d번 게시물의 제목이 수정되었습니다\n", id);
+
+				} else if (command_edit.equals("내용")) {
+					System.out.printf("%d번 게시물의 내용을 수정합니다\n", id);
+					System.out.printf("내용 : ");
+					String body = sc.nextLine();
+					foundArticle.body = body;
+					
+					LocalDate lastdate = LocalDate.now();
+					String LastEditDate = lastdate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+					LocalTime lasttime = LocalTime.now();
+					String LastEditTime = lasttime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+					
+					foundArticle.LastEditDate = LastEditDate;
+					foundArticle.LastEditTime = LastEditTime;
+					
+					System.out.printf("%d번 게시물의 내용이 수정되었습니다\n", id);
+					
+				} else {
+					System.out.println("'제목' 혹은 '내용'을 입력해주세요");
+				}
 
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다.");
@@ -92,7 +181,7 @@ public class Main {
 
 		sc.close();
 	}
-} 
+}
 
 class Article {
 	int id;
@@ -100,6 +189,8 @@ class Article {
 	String body;
 	String NowDate;
 	String NowTime;
+	String LastEditDate;
+	String LastEditTime;
 
 	Article(int id, String title, String body, String NowDate, String NowTime) {
 		this.id = id;
